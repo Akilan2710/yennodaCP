a264e76c03810c965725d534e2a49c24ebede61c
Akilan <akilan.tmlzs@gmail.com>
2026-07-22 19:27:09 +0530

    c

--- codeforces/B_Cake_Leveling.cpp
+++ codeforces/B_Cake_Leveling.cpp
@@ -10,17 +10,13 @@ int main() {
     while(t--){
        int n;
         cin >> n;
-
         vector<long long> a(n);
         for(int i=0; i<n; i++) cin >> a[i];
-
         long long ans = 1e9, sum = 0;
-
         for(int i=0; i<n; i++)
         {
             sum += a[i];
             ans = min(ans, sum / (i + 1));
-
             cout << ans << " ";
         }
         cout << "\n"; 
