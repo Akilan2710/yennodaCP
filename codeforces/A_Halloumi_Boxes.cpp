3c8b7095c178a5190c301bea47673d13bf6615c2
Akilan <akilan.tmlzs@gmail.com>
2026-09-15 20:31:32 +0530

    1

---
+++ codeforces/A_Halloumi_Boxes.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        for(int i = 0;i < n;i++){
+            cin>>arr[i];
+        }
+        if(is_sorted(arr,arr+n) || k > 1){
+            cout<<"YES\n";
+        }
+        else{
+            cout<<"NO\n";
+        }
+    }
+    return 0;
+}
